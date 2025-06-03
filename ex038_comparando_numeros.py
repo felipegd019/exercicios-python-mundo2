280763381f1ab99fd9b7fb3ac826d74d2548eff7
felipegd019 <gd.especulador@gmail.com>
2025-06-03 11:03:27 -0300

    Add files via upload

---
+++ ex038_comparando_numeros.py
@@ -0,0 +1,9 @@
+num1 = int(input('Primeiro numero: '))
+num2 = int(input('Segundo numero: '))
+
+if num1 > num2:
+    print(f'O número {num1} é maior')
+elif num2 > num1:
+    print(f'O número {num2} é maior')
+else:
+    print('Os dois valores são iguais')
