d43a5e1b869e4de102f6adfdb8aa65a7fafa03d9
Felipe Gomes <gd.especulador@gmail.com>
2025-06-13 12:09:53 -0300

    Update ex048_soma_impares_multiplos_de_tres.py

--- ex048_soma_impares_multiplos_de_tres.py
+++ ex048_soma_impares_multiplos_de_tres.py
@@ -1,14 +1,8 @@
 somar = 0
+cont = 0
 for i in range(1, 501):
     if  i % 3 == 0 and i % 2 != 0:
+        cont = cont + 1 
         somar = somar + i
     
-print(f'A soma de todos os 83 valores solicitados é {somar}')
-
-
-
-
-
-
-
-    
+print(f'A soma de todos os {cont} valores solicitados é {somar}')
