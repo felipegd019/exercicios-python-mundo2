cbf7a337d55d32cd1e46d68a8e7379d27510122e
Felipe Gomes <gd.especulador@gmail.com>
2025-06-13 11:57:14 -0300

    Add files via upload

---
+++ ex048_soma_impares_multiplos_de_tres.py
@@ -0,0 +1,14 @@
+somar = 0
+for i in range(1, 501):
+    if  i % 3 == 0 and i % 2 != 0:
+        somar = somar + i
+    
+print(f'A soma de todos os 83 valores solicitados é {somar}')
+
+
+
+
+
+
+
+    
