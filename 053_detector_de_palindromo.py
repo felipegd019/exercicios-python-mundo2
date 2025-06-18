b138851e699cb681abcfca5b7423ca222a376de4
Felipe Gomes <gd.especulador@gmail.com>
2025-06-18 12:01:59 -0300

    Delete 053_detector_de_palindromo.py

--- 053_detector_de_palindromo.py
+++
@@ -1,8 +0,0 @@
-frase = str(input('Digite uma frase: '))
-frase = frase.replace(' ', '')
-frase = frase.lower()
-
-if  frase[::-1] == frase:
-    print('É um palindromo')
-else: 
-    print('Nao é um palindromo')
