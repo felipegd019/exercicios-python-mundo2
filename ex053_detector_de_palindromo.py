19b79d73aad624bb0ab217ec8d3caaee12aaff5e
Felipe Gomes <gd.especulador@gmail.com>
2025-06-19 11:32:39 -0300

    Update ex053_detector_de_palindromo.py

--- ex053_detector_de_palindromo.py
+++ ex053_detector_de_palindromo.py
@@ -2,7 +2,13 @@ frase = str(input('Digite uma frase: '))
 frase = frase.replace(' ', '')
 frase = frase.lower()
 
-if  frase[::-1] == frase:
-    print('É um palindromo')
-else: 
-    print('Nao é um palindromo')
+ultima = len(frase)-1
+invertida = ''
+
+for i in range(ultima ,-1 ,-1):
+    invertida = invertida + frase[i]
+    
+if frase == invertida:
+    print(f'A frase {frase} é um palindromo')
+else:
+    print(f'A frase {frase} náo é um palindromo')
