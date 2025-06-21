2844b783a79f437b7dfa6b5f8d7d5f38b7f7c225
Felipe Gomes <gd.especulador@gmail.com>
2025-06-21 12:22:37 -0300

    Add files via upload

--- ex054_grupo_da_maioridade.py
+++ ex054_grupo_da_maioridade.py
@@ -1,13 +1,14 @@
-ano_atual = 2025
-maiores = 0 
+from datetime import date 
+
+maiores = 0
 menores = 0
+ano_atual = date.today().year 
 
 for i in range(1, 8):
     ano = int(input(f'Em que ano a {i}ª pessoa nasceu? '))
-    if  ano_atual - ano >= 18:
+    if ano_atual - ano >= 18:
         maiores = maiores + 1
     else:
-        menores = menores + 1 
+        menores = menores + 1
 
 print(f'Existem {maiores} maiores de idade, e {menores} menores de idade!')
-
